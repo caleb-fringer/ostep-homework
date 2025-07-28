17801f24b8fcca66ab9c496a9e3ea34c1182274b
Caleb Fringer <cfringer98@gmail.com>
2025-07-28 16:04:28 -0700

    Implement basic concurrent counter.

---
+++ threads-data-structures/counter.h
@@ -0,0 +1,12 @@
+#include <pthread.h>
+
+typedef struct __counter_t {
+    int count;
+    pthread_mutex_t lock;
+} counter_t;
+
+void init(counter_t *counter);
+void reset(counter_t *counter);
+int getCount(counter_t *counter);
+void increment(counter_t *counter);
+void decrement(counter_t *counter);
