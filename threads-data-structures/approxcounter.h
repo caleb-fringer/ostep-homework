980b1196435cb8d41f3d6397606d69d271579620
Caleb Fringer <cfringer98@gmail.com>
2025-07-29 14:30:59 -0700

    Define approx_counter_t interface.

---
+++ threads-data-structures/approxcounter.h
@@ -0,0 +1,24 @@
+#ifndef __APROX_COUNTER_H
+#define __APROX_COUNTER_H
+
+#include <pthread.h>
+
+#define NUMCPUS 12
+#define DEFAULT_THRESHOLD 5
+
+typedef struct {
+    int threshold;
+
+    int global_count;
+    pthread_mutex_t global_lock;
+
+    int local_count[NUMCPUS];
+    pthread_mutex_t local_lock[NUMCPUS];
+} approx_counter_t;
+
+void init(approx_counter_t *counter);
+void reset(approx_counter_t *counter);
+void update(approx_counter_t *counter, int amount);
+int getCount(approx_counter_t);
+
+#endif
