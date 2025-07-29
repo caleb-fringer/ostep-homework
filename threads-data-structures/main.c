9cf56fafe834764ccd048e4cc0b6d89305935441
Caleb Fringer <cfringer98@gmail.com>
2025-07-28 19:22:06 -0700

    Implemented data structure agnostic trial framework.

--- threads-data-structures/main.c
+++ threads-data-structures/main.c
@@ -1,5 +1,8 @@
 #include <assert.h>
+#include <bits/time.h>
+#include <pthread.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
 
 #include "counter.h"
@@ -8,21 +11,27 @@ typedef union {
     counter_t counter;
 } concurrent_struct_t;
 
+typedef enum { COUNTER, LINKED_LIST, QUEUE, B_TREE } data_struct_types;
+
 typedef struct {
+    data_struct_types type;
     concurrent_struct_t *data_struct;
-    int num_threads;
     int num_iterations;
     void *(*method)(void *);
 } trial_params_t;
 
 double getElapsedTime(struct timespec *start, struct timespec *end);
-double trial(trial_params_t *params);
+double trial(int num_threads, trial_params_t *params);
+void *threadRoutine(void *params);
 
 int main() {
-    struct timespec begin_time;
-    assert(clock_gettime(CLOCK_MONOTONIC_RAW, &begin_time) == 0);
-
-    printf("Time: %lds %ldns\n", begin_time.tv_sec, begin_time.tv_nsec);
+    trial_params_t trial_1 = {.type = COUNTER,
+                              .data_struct = &(concurrent_struct_t){},
+                              .num_iterations = 10000000,
+                              .method = (void *(*)(void *)) & increment};
+    double time = trial(12, &trial_1);
+    printf("It took %fs to run %d add operations.\n", time,
+           trial_1.num_iterations);
     return 0;
 }
 
@@ -53,3 +62,40 @@ double getElapsedTime(struct timespec *start, struct timespec *end) {
  * passed, K, the data structure used, and a function pointer to the operation
  * I want to perform on it.
  */
+double trial(int num_threads, trial_params_t *params) {
+    // Initialize the data structure
+    switch (params->type) {
+    case COUNTER:
+        init((counter_t *)params->data_struct);
+        break;
+    default:
+        perror("Error initializng data structure, unknown or unimplemented "
+               "type.\n");
+    }
+
+    pthread_t *threads = malloc(sizeof(pthread_t) * num_threads);
+
+    struct timespec begin, end;
+    assert(clock_gettime(CLOCK_MONOTONIC_RAW, &begin) == 0);
+
+    for (int t = 0; t < num_threads; t++) {
+        pthread_create(&threads[t], NULL, &threadRoutine, params);
+    }
+
+    for (int t = 0; t < num_threads; t++) {
+        pthread_join(threads[t], NULL);
+    }
+
+    assert(clock_gettime(CLOCK_MONOTONIC_RAW, &end) == 0);
+    free(threads);
+
+    return getElapsedTime(&begin, &end);
+}
+
+void *threadRoutine(void *p) {
+    trial_params_t *params = (trial_params_t *)p;
+    for (int i = 0; i < params->num_iterations; i++) {
+        params->method(params->data_struct);
+    }
+    return NULL;
+}
